3b9f41bbb361852c17d4e1820c1db405b21e6abc
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-01-19 17:53:13 -0300

    Create 022_TEXT_TRANSFORMER.py

---
+++ 022_TEXT_TRANSFORMER.py
@@ -0,0 +1,9 @@
+name = input("Input whole name: ")
+print(name.upper())
+print(name.lower())
+namesplit=name.split()
+namejoin="".join(namesplit)
+print(len(namejoin))
+print(len(namesplit[0]))
+input()
+
