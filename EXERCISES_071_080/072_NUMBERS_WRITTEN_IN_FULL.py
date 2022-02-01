5138306900cb4c42a3dec32cf7665565fcd2300a
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-01 18:50:25 -0300

    Create 072_NUMBERS_WRITTEN_IN_FULL.py

---
+++ EXERCISES_071_080/072_NUMBERS_WRITTEN_IN_FULL.py
@@ -0,0 +1,8 @@
+# statements
+numberswritten = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                  "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+                  "nineteen", "twenty")
+#input
+while True:
+    number = int(input("Type a number from 0 to 20: "))
+    print(f"You typed the number {numberswritten[number]}.")
