00296e151ebd17a0be0a820a65a263856a56be49
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-04 22:49:33 -0300

    Create 091_DICE_GAMES.py

---
+++ EXERCISES_091_100/091_DICE_GAMES.py
@@ -0,0 +1,23 @@
+# library
+from time import sleep
+from random import randint
+from operator import itemgetter
+
+# statements
+play = dict()
+allgame = list()
+ranking = dict()
+# output, processing
+for count in range(1, 5):
+    sleep(0.5)
+    result = randint(1, 6)
+    print(f"\033[1;38mThe player {count} rolled \033[1;33m{result} \033[1;38min the dice. ")
+    play[f'gamer {count}'] = result
+allgame.append(play)
+print("-=" * 30)
+
+print(f"{'Ranking of the gamers ':=^30}")
+ranking = sorted(play.items(), key=itemgetter(1), reverse=True)
+for index, count in enumerate(ranking):
+    sleep(0.5)
+    print(f"  {index+1}º place: {count[0]} with {count[1]}")
