d197504c5536c8ca3c6f23d868ac0c3cc037cd66
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-08 21:08:27 -0300

    Exercise 96

--- EXERCISES_091_100/096_LAND_AREA_CALCULATOR.py
+++ EXERCISES_091_100/096_LAND_AREA_CALCULATOR.py
@@ -1,11 +1,15 @@
 # function
-def area(a,b):
-    print(f"The area of a land of \033[1;34m{a} \033[1;38mx \033[1;34m{b} \033[1;38mis \033[1;33m{a*b} m².")
+def area(a, b):
+    print(f"The area of a land of \033[1;34m{a} \033[1;38mx "
+          f"\033[1;34m{b} \033[1;38mis \033[1;33m{a * b} m².")
+
 
 # enhancement
-print("\033[1;38m-="*25)
+print("\033[1;38m-=" * 25)
 print(f"{'Area of lands':^50}")
-print("-="*25)
+print("-=" * 25)
+# input
 width = float(input("Input the land's width in meters: "))
 length = float(input("Input the land's length in meters: "))
+# output
 area(width, length)
---
+++ EXERCISES_091_100/097_SPECIAL_WRITER.py
@@ -0,0 +1,12 @@
+# function
+def writer(sentence):
+    print("~" * (len(sentence) + 10))
+    print(f"    {sentence}")
+    print("~" * (len(sentence) + 10))
+
+
+# input
+phrase = input("Input a phrase to be enhanced: ")
+
+# output
+writer(phrase)
