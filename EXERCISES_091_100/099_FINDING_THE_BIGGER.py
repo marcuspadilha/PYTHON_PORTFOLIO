ccd81764786d20616ffc25cdd4cf5161214dccb9
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-08 22:08:54 -0300

    Update 099_FINDING_THE_BIGGER.py

--- EXERCISES_091_100/099_FINDING_THE_BIGGER.py
+++ EXERCISES_091_100/099_FINDING_THE_BIGGER.py
@@ -6,18 +6,21 @@ numbers = list()
 # function
 def bigger(lst):
     print("-=" * 20)
-    print("Examining the values entered: ")
-    print(f"A) The values are ", end=" ")
+    print("\033[1;34mExamining the values entered: ")
+    sleep(0.6)
+    print(f"\033[1;38mA) The values are \033[1;33m", end=" ")
     for count in lst:
         print(count, end= " ")
-    print(".")
-    print(f"B) It was typed {len(lst)} values.")
-    print(f"C) The biggest value is {max(lst)}.")
+    print("\033[1;38m.")
+    sleep(0.6)
+    print(f"B) It was typed \033[1;33m{len(lst)} \033[1;38mvalues.")
+    sleep(0.6)
+    print(f"C) The biggest value is \033[1;33m{max(lst)}\033[1;38m.")
 
 
 # input
 while True:
-    number = int(input("Input a number: "))
+    number = int(input("\033[1;38mInput a number: "))
     numbers.append(number)
     choice = input("Do you want to continue? ").strip().upper()
     if choice == "N":
