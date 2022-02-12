7ac2448d98310b7a2f2b204abefa7f9cdd6370d4
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-11 22:13:44 -0300

    Exercise 101

---
+++ EXERCISES_101_110/VOTE_ANALYSIS.py
@@ -0,0 +1,16 @@
+# function
+def vote(num):
+    from datetime import date
+    currentyear = date.today().year
+    if (currentyear - number) < 16:
+        return f"Being {currentyear-number}, the person does not vote."
+    elif 16 <= (currentyear - number) < 18:
+        return f"Being {currentyear - number}, the vote is optional."
+    elif 18 <= (currentyear - number) < 60:
+        return f"Being {currentyear - number}, the vote is mandatory."
+    else:
+        return f"Being {currentyear - number}, the vote is optional."
+
+# input
+number = int(input("\033[1;38mEnter birth year: "))
+print(vote(number))
