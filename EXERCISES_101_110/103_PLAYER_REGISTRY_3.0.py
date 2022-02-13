ed36395660244c0140c32cd82d60a882bb38d77b
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-12 23:02:16 -0300

    Create 103_PLAYER_REGISTRY_3.0.py

---
+++ EXERCISES_101_110/103_PLAYER_REGISTRY_3.0.py
@@ -0,0 +1,18 @@
+# function
+def player(name, goals):
+    return f"The player {name} scored {goals} goals. "
+
+
+# input
+firstname = input("Enter the player's name: ").strip().capitalize()
+numbergoals = input("Enter the number of goals: ")
+
+if numbergoals.isnumeric():
+    numbergoals = int(numbergoals)
+else:
+    numbergoals = 0
+
+if firstname == "":
+    firstname = "Unknown"
+
+print(player(firstname, numbergoals))
