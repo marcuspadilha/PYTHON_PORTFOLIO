f69317624f2363ed54280e029ca3acc916f1691a
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-16 21:51:59 -0300

    107_FUNCTIONS

---
+++ EXERCISES_101_110/107_FUNCTIONS_IN_MODULES/FUNCTIONS.py
@@ -0,0 +1,8 @@
+def half(num1):
+    return f"The half of the value is $ {num1/2}."
+
+def double(num2):
+    return f"The double of the value is $ {num2*2}."
+
+def adding10(num3):
+    return f"Add 10% in the value we get $ {num3*1.1}."
