50b5b90bf2980b78123638fdbf0aa993a6011d2a
Marcus Padilha <marcuspadilha1106@gmail.com>
2022-02-16 22:13:03 -0300

    109_FORMATTING_CURRENCE

---
+++ EXERCISES_101_110/109_FORMATTING_CURRENCE_2.0/FUNCTIONS.py
@@ -0,0 +1,11 @@
+def half(num1):
+    return f"The half of the value is R$ {str(f'{num1/2:.2f}').replace('.',',')}."
+
+def double(num2):
+    return f"The double of the value is R$ {str(f'{num2*2:.2f}').replace('.',',')}."
+
+def adding10(num3):
+    return f"Add 10% in the value we get R$ {str(f'{num3*1.1:.2f}').replace('.',',')}."
+
+
+
---
+++ EXERCISES_101_110/109_FORMATTING_CURRENCE_2.0/MAIN_PROGRAM.py
@@ -0,0 +1,10 @@
+# library
+import FUNCTIONS
+
+# input
+value = float(input("\033[1;38mEnter with the value: R$ "))
+print()
+print(FUNCTIONS.half(value))
+print(FUNCTIONS.double(value))
+print(FUNCTIONS.adding10(value))
+input()
